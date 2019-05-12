1572806d0517b3a23260ac2f0f3dc77a2ae17abb
emmanue1 <java.decompiler@gmail.com>
2019-05-12 23:45:48 +0200

    Publish JD-GUI 1.4.3 and remove references to JD-IntelliJ

--- website/src/js/jd.js
+++ website/src/js/jd.js
@@ -6,7 +6,6 @@ var _gaq = [['_setAccount', 'UA-4144628-1'],
 		// Google Analytics
 		$('#jd-project-overview-tab')		            .on('mousedown', function() { _gaq.push(['_trackPageview', '/jd-project-overview']); });
 		$('#jd-project-main-features-tab')	            .on('mousedown', function() { _gaq.push(['_trackPageview', '/jd-project-main-features']); });
-		$('#jd-gui-jd-core-preview-tab')	            .on('mousedown', function() { _gaq.push(['_trackPageview', '/jd-core-1.0.0-preview']); });
 
 		$('#jd-gui-overview-tab')						.on('mousedown', function() { _gaq.push(['_trackPageview', '/jd-gui-overview']); });
 		$('#jd-gui-download-tab')						.on('mousedown', function() { _gaq.push(['_trackPageview', '/jd-gui-download']); });
@@ -15,9 +14,6 @@ var _gaq = [['_setAccount', 'UA-4144628-1'],
 		$('#jd-eclipse-overview-tab')					.on('mousedown', function() { _gaq.push(['_trackPageview', '/jd-eclipse-overview']); });
 		$('#jd-eclipse-download-tab')					.on('mousedown', function() { _gaq.push(['_trackPageview', '/jd-eclipse-download']); });
 		$('#jd-eclipse-download input[type="image"]')	.on('mousedown', function() { _gaq.push(['_trackEvent', 'JD-Eclipse', 'Paypal']); });
-
-		$('#jd-intellij-overview-tab')					.on('mousedown', function() { _gaq.push(['_trackPageview', '/jd-intellij-overview']); });
-		$('#jd-intellij-download-tab')					.on('mousedown', function() { _gaq.push(['_trackPageview', '/jd-intellij-install']); });
 	});			
 })(jQuery);
 
